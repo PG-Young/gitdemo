784a311f0ffd66f9ecd58b4142db5d3ff255fb89
yangweihua <weihua@itcast.cn>
2019-04-27 19:24:13 +0800

    add new Class 2

---
+++ src/main/java/com/atguigu/gitdemo/AddNewBranch.java
@@ -0,0 +1,14 @@
+package com.atguigu.gitdemo;
+
+/**
+ * @ClassName AddNewBranch
+ * @Description TODO
+ * @Author wehua young
+ * @Date 2019/4/27 19:23
+ * @Version 1.0.0
+ **/
+public class AddNewBranch {
+    public static void main(String[] args) {
+        System.out.println("分支开发");
+    }
+}
