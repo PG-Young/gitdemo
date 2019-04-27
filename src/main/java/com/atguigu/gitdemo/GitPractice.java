e491f3013e1f4829601575f63570a07f3d0703d7
yangweihua <weihua@itcast.cn>
2019-04-27 18:36:22 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/gitdemo/GitPractice.java
@@ -0,0 +1,14 @@
+package com.atguigu.gitdemo;
+
+/**
+ * @ClassName GitPractice
+ * @Description TODO
+ * @Author wehua young
+ * @Date 2019/4/27 18:31
+ * @Version 1.0.0
+ **/
+public class GitPractice {
+    public static void main(String[] args) {
+        System.out.println("practice demo...");
+    }
+}
